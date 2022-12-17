fb70bf54fe3bd05e842eff49b01e2bb948e9022d
Ruşen Ali Yılmaz <36644037+LikeGamerTr@users.noreply.github.com>
2022-12-17 21:37:04 +0300

    Add files via upload

---
+++ bg.py
@@ -0,0 +1,19 @@
+import os
+import time
+import random
+
+while True:
+    # Set the path to the directory containing the images you want to use as your desktop background
+    image_directory = '/home/marhpad/Pictures/Wallpaper'
+
+    # Get a list of all the images in the directory
+    images = os.listdir(image_directory)
+
+    # Choose a random image from the list
+    image = images[int(random.random() * len(images))]
+
+    # Set the image as the desktop background
+    os.system(f'gsettings set org.gnome.desktop.background picture-uri file://{image_directory}/{image}')
+
+    # Wait for a period of time before changing the background again
+    time.sleep(45) # Change the background every 15s
